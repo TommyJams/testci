a15f7c8668a168bfdc86d20590cff1e87c7e7ee5
TommyJams <tommyjams.bizspark@outlook.com>
2013-09-23 16:21:06 +0530

    campaign 3

--- application/models/model.php
+++ application/models/model.php
@@ -67,28 +67,29 @@ class Model extends CI_Model{
    				$target = $row->target;
    				$raised = $row->raised;
    				$endCamp = $row->endCamp;
+
+		   		$todayDate = date("Y-m-d");	
+   				$diff = abs(strtotime($endCamp) - strtotime($todayDate));
+				$years = floor($diff / (365*60*60*24));
+				$months = floor(($diff - $years * 365*60*60*24) / (30*60*60*24));
+				$days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
+
+				$funded = (( $raised/$target ) * 100);
+				$days_to_go = $days;
+
+				$campaignRow = array(
+										'campaign_id' => $campaign_id,
+										'artist_id'   => $artist_id,
+										'artist_name' => $artist_name,
+										'funded'      => $funded,
+										'days_to_go'  => $days_to_go
+									);
+							
+				$response['campaignRow'][] = $campaignRow;
    			}
    		}
-   			
-   		$todayDate = date("Y-m-d");	
-   		$diff = abs(strtotime($endCamp) - strtotime($todayDate));
-		$years = floor($diff / (365*60*60*24));
-		$months = floor(($diff - $years * 365*60*60*24) / (30*60*60*24));
-		$days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
 
-		$funded = (( $raised/$target ) * 100);
-		$days_to_go = $days;
-
-		$campaignRow = array(
-								'campaign_id' => $campaign_id,
-								'artist_id'   => $artist_id,
-								'artist_name' => $artist_name,
-								'funded'      => $funded,
-								'days_to_go'  => $days_to_go
-							);
-							
-		$response['campaignRow'][] = $campaignRow;	
-
+   		//Return values to controller
 		return $response;        
 	}
 
@@ -97,53 +98,88 @@ class Model extends CI_Model{
 		$query = $this->db->query("SELECT * FROM campaignCF;");
 		if ($query->num_rows() > 0)
 		{
-			foreach ($query->result() as $row)
+			$qresult = $query->result();
+			foreach ($qresult as $row)
 			{
-				$campaign_id = $row['campaign_id'];$tour_id = $row['tour_id'];$tour_name = $row['tour_name'];
-   				$artist_id = $row['artist_id'];$artist_name = $row['artist_name'];$deadline = $row['deadline'];
-   				$target = $row['target'];$raised = $row['raised'];$totalPledges = $row['totalPledges'];
-   				$startCamp = $row['startCamp'];$endCamp = $row['endCamp'];$videoLink = $row['videoLink'];
-   				$fb = $row['fb'];$twitter = $row['twitter'];$website = $row['website'];
-   				$scloud = $row['soundcloud'];$rever = $row['reverbnation'];$youtube = $row['youtube'];
-   				$myspace = $row['myspace'];$image1 = $row['image1'];$image2 = $row['image2'];
-   				$image3 = $row['image3'];$status = $row['status'];$tourDate = $row['tourDate'];
+				$campaign_id = $row->campaign_id;$tour_id = $row->tour_id;$tour_name = $row->tour_name;
+   				$artist_id = $row->artist_id;$artist_name = $row->artist_name;$deadline = $row->deadline;
+   				$target = $row->target;$raised = $row->raised;$totalPledges = $row->totalPledges;
+   				$startCamp = $row->startCamp;$endCamp = $row->endCamp;$videoLink = $row->videoLink;
+   				$fb = $row->fb;$twitter = $row->twitter;$website = $row->website;
+   				$scloud = $row->soundcloud;$rever = $row->reverbnation;$youtube = $row->youtube;
+   				$myspace = $row->myspace;$image1 = $row->image1;$image2 = $row->image2;
+   				$image3 = $row->image3;$status = $row->status;$tourDate = $row->tourDate;
 
    				// 3D array formation; Getting amount details
    				$query1 = $this->db->query("SELECT * FROM pledgeCF WHERE campaign_id = '$campaign_id';");
 				if ($query1->num_rows() > 0)
 				{
-					foreach ($query1->result() as $row)
+					$q1result = $query1->result();
+					foreach ($q1result as $rowPledge)
 					{
-						$amount = $row['amount'];
-						$desc = $row['desc'];
+						$amount = $row->amount;
+						$desc = $row->desc;
+
+						$pledge[] = $rowPledge;
 					}
 				}
 
 				// 3D array formation; Getting venue details
-				$query2 = $this->db->query("SELECT * FROM pledgeCF WHERE tour_id = '$tour_id';");
+				$query2 = $this->db->query("SELECT * FROM venueCF WHERE tour_id = '$tour_id';");
 				if ($query2->num_rows() > 0)
 				{
-					foreach ($query2->result() as $row)
+					$q2result = $query2->result();
+					foreach ($q2result as $rowVenue)
 					{
-						$venue_name = $row['venue_name'];
-						$image = $row['image'];
-						$desc = $row['desc'];
-						$link = $row['link'];	
-						$city = $row['city'];
-						$contact = $row['contact'];
+						$venue_name = $row->venue_name;
+						$image = $row->image;
+						$desc = $row->desc;
+						$link = $row->link;	
+						$city = $row->city;
+						$contact = $row->contact;
+
+						$venues[] = $rowVenue;
+
 					}
 				}
+   				
+   				$todayDate = date("Y-m-d");	
+   				$diff = abs(strtotime($endCamp) - strtotime($todayDate));
+				$years = floor($diff / (365*60*60*24));
+				$months = floor(($diff - $years * 365*60*60*24) / (30*60*60*24));
+				$days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
+				$days_to_go = $days;
+
+				$campaignDetails = array(
+											'campaign_id' 	=> $campaign_id,
+											'videoLink'		=> $videoLink,
+											'raised' 		=> $raised,
+											'target' 		=> $target,
+											'artist_id'   	=> $artist_id,
+											'artist_name' 	=> $artist_name,
+											'venues' 		=> $venues,
+											'pledge' 		=> $pledge,
+											'fb' 			=> $fb,
+											'twitter' 		=> $twitter,
+											'website' 		=> $website,
+											'scloud' 		=> $scloud,
+											'rever' 		=> $rever,
+											'youtube' 		=> $youtube,
+											'myspace' 		=> $myspace,
+											'image1' 		=> $image1,
+											'image2' 		=> $image2,
+											'image3' 		=> $image3,
+											'status' 		=> $status,
+											'tourDate' 		=> $tourDate,
+											'days_to_go'  	=> $days_to_go
+										);
+							
+				$response[] = $campaignDetails;
    			}
    		}
-   			
-   		$todayDate = date("Y-m-d");	
-   		$diff = abs(strtotime($endCamp) - strtotime($todayDate));
-		$years = floor($diff / (365*60*60*24));
-		$months = floor(($diff - $years * 365*60*60*24) / (30*60*60*24));
-		$days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
-		$days_to_go = $days;
-
-		// In which format and how to return values to controller
+		
+		//Return values to controller
+		return $response;
 	}
 }
 ?>
