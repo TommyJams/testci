f5f0ca0e97ee6e0acf2ff5b68f5b4c3d2b6e5e10
TommyJams <tommyjams.bizspark@outlook.com>
2013-09-24 13:28:11 +0530

    tour 30

--- application/views/tours_view.php
+++ application/views/tours_view.php
@@ -50,7 +50,7 @@
           <div class="row text-center">
             <div class="col-md-6 col-sm-6  d-tj-tour-left" > 
             <div style="background:black">
-            <img src="img/temp/$image" alt="">
+            <img src="img/temp/<? print($image); ?>" alt="">
             </div>
               <h4>
               <span ><? print($venue_name); ?></span> <? print($city); ?></h4>
