7918009da97e9e777e54b1e9a6e63b3f77be866f
TommyJams <tommyjams.bizspark@outlook.com>
2013-09-29 17:25:30 +0530

    camp edit 10

--- application/views/campaignedit_view.php
+++ application/views/campaignedit_view.php
@@ -37,9 +37,9 @@
         <div class="row d-tj-tour">
           <div class="col-sm-12 col-xs-12 col-md-7 d-tj-video-edit">
             <div class="d-tj-video-edit-bdr">
-                <h2><a  data-toggle="modal" href="#videoModal" title="VideoLink" alt="" target="_blank" 
+                <a  data-toggle="modal" href="#videoModal" title="VideoLink" alt="" target="_blank" 
                         class="social-list-facebook-edit openform">VIDEO LINK</a>
-                </h2>
+                
             </div>
           </div>
           <div class="col-sm-12 col-md-5 d-tj-black-box-container" >
