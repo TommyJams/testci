838802f38ba037549130b6097047ed61528ae832
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-01 16:02:23 +0530

    camp edit 72

--- application/views/campaignedit_view.php
+++ application/views/campaignedit_view.php
@@ -288,9 +288,10 @@ $(".add-option").click(function(){
 var maxIndex = x + 1; 
 var a = 'pledgeAmount' + maxIndex;
 var b = 'PLEDGE AMT' + maxIndex;
+document.getElementById("pholder").innerHTML=b;
 var desc = 'desc' + maxIndex;
 
-var addoption = '<div id="t1" class="pledge"><h4><input  class="form-control input-lg pull-left" type="text" name="document.write(a);" placeholder="document.write(b)"><i class=" btn-delete-pledge pull-right">-</i></h4><div class="clearfix"></div>';
+var addoption = '<div id="t1" class="pledge"><h4><input  class="form-control input-lg pull-left" type="text" name="document.write(a);" placeholder="<p id="pholder"></p>"><i class=" btn-delete-pledge pull-right">-</i></h4><div class="clearfix"></div>';
 addoption +='<h5> <textarea class="form-control" name="document.write(desc);" placeholder="Please write description" rows="4" ></textarea></h5>';
 addoption +=' <div class="seperator" ></div></div></div>';
 
