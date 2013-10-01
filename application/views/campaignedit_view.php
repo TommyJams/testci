7f08553be18a80c2820f713cdd0dbf9cfe180c44
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-01 17:18:51 +0530

    camp edit 74

--- application/views/campaignedit_view.php
+++ application/views/campaignedit_view.php
@@ -293,7 +293,7 @@ var desc = 'desc' + maxIndex;
 
 var pholder = '<p id="pholder"></p>';
 
-var addoption = '<div id="t1" class="pledge"><h4><input  class="form-control input-lg pull-left" type="text" name="document.write(a);" placeholder="document.writeln(pholder)"><i class=" btn-delete-pledge pull-right">-</i></h4><div class="clearfix"></div>';
+var addoption = '<div id="t1" class="pledge"><h4><input  class="form-control input-lg pull-left" type="text" name="document.write(a);" placeholder="document.writeln(b)"><i class=" btn-delete-pledge pull-right">-</i></h4><div class="clearfix"></div>';
 addoption +='<h5> <textarea class="form-control" name="document.write(desc);" placeholder="Please write description" rows="4" ></textarea></h5>';
 addoption +=' <div class="seperator" ></div></div></div>';
 
