05063f160b0f2833002da7724a9a173de696c80e
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-04 21:14:34 +0530

    edit 44

--- application/views/campaignedit_view.php
+++ application/views/campaignedit_view.php
@@ -29,7 +29,7 @@
       <h4>Add  Video Link</h4>
     </div>
     <div class="modal-body modal-link">
-      <input type="text" id="videolink" name="videolink" class="input-lg">
+      <input type="text" value="" id="videolink" name="videolink" class="input-lg">
     </div>
     <div class="modal-footer">
       <a href="javascript:;" onclick="$.fancybox.close();" class="btn blk-btn" data-dismiss="modal">Submit</a> 
