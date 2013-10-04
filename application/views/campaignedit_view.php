ab21b64b885f9edce40fd8c7e33b6d42bea21268
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-04 12:37:12 +0530

    edit 7

--- application/views/campaignedit_view.php
+++ application/views/campaignedit_view.php
@@ -377,7 +377,8 @@ $('body').on('click', '.btn-delete-pledge', function(){
 
         //var blah = elem.closest('.video-link');
 
-        var vlink = document.getElementById('vlink').value;
+        //var vlink = document.getElementById('vlink').value;
+        var vlink = document.querySelector('#vlink').value;
         //var vlink = elem.closest('.video-link').children('.input-lg').val();
         console.log(vlink);
         $('input[name=vlink]').val(vlink);
