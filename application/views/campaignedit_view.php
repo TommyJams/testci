88f40e08fc7697e48432f938aae4404424b32ad3
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-04 15:18:37 +0530

    edit 19

--- application/views/campaignedit_view.php
+++ application/views/campaignedit_view.php
@@ -29,10 +29,10 @@
       <h4>Add  Video Link</h4>
     </div>
     <div class="modal-body modal-link">
-      <input type="text" id="videolink" name="vlink" class="input-lg">
+      <input type="text" value ="" id="videolink" name="vlink" class="input-lg">
     </div>
     <div class="modal-footer">
-      <a href="javascript:;" onchange="insertLinks(this.value);" class="btn blk-btn" data-dismiss="modal">Submit</a> 
+      <a href="javascript:;" onclick="insertLinks(this.value);" class="btn blk-btn" data-dismiss="modal">Submit</a> 
     </div>
   </div>
 </div>
