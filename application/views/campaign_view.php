61a697d86b63660df2ce35247b69e5b728f241b6
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-07 20:52:40 +0530

    Revert "fbshare button"
    This reverts commit b49907a5f322bd714d520c9e02637d096c098f83.

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -144,7 +144,6 @@
               <input type="button" value="PLEDGE NOW" style="">
             </div>
           </div>
-          <div class="fb-post" data-href="https://www.facebook.com/FacebookDevelopers/posts/10151471074398553"></div>
         </div>
         <!--  /Pledge--> 
       </div>
@@ -185,30 +184,6 @@ $('#foo5').carouFredSel({
 				});
 	});
 </script> 
-
-<script>
-  window.fbAsyncInit = function() {
-    // init the FB JS SDK
-    FB.init({
-      appId      : '248776888603319',                        // App ID from the app dashboard
-      channelUrl : '//testcodeigniter.azurewebsites.net/channel.html', // Channel file for x-domain comms
-      status     : true,                                 // Check Facebook Login status
-      xfbml      : true                                  // Look for social plugins on the page
-    });
-
-    // Additional initialization code such as adding Event Listeners goes here
-  };
-
-  // Load the SDK asynchronously
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/all.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
-
 <!--<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script> -->
 <script type="text/javascript" src="/script/jquery.supersized.min.js"></script> 
 <script type="text/javascript" src="/script/jquery.supersized.shutter.min.js"></script> 
