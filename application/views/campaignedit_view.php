b7d6f9ab01ad1019c48cd4a43c45a3e757f9a98e
TommyJams <nikhilk218@gmail.com>
2013-10-08 19:55:30 +0530

    add option help 7

--- application/views/campaignedit_view.php
+++ application/views/campaignedit_view.php
@@ -286,12 +286,12 @@
           </h5>
           <div class="seperator" ></div>
         </div>
-        <div id="add-option" ></div>
-        <div  class="add-option"><h4><a ><img src="/img/add.png" alt="" style=""> ADD OPTION</a><h4>
+        <div class="add-option pull-left"><h4><a ><img src="/img/add.png" alt="" style=""> ADD OPTION</a><h4>
         </div>
-        <a  class="open-help-form pull-right" data-toggle="modal" href="#helpModal" target="_blank" class="social-list-facebook-edit open-help-form">
+        <a class="open-help-form pull-right" data-toggle="modal" href="#helpModal" target="_blank" class="social-list-facebook-edit open-help-form">
             <img src="/img/help.png" alt="" style="">
         </a>
+        <div class="clearfix"></div>
       </div>
       <div class=" d-tj-offset-top-30 pledge-btn" >
       <input type="hidden" id="tour_id"     name="tour_id"    value="<? print($tour_id); ?>" /> 
