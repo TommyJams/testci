bae258c4c88f9b2e8c9736ef33ee9af3eec5dae1
TommyJams <nikhilk218@gmail.com>
2013-10-08 19:45:58 +0530

    add option help 4

--- application/views/campaignedit_view.php
+++ application/views/campaignedit_view.php
@@ -286,6 +286,7 @@
           </h5>
           <div class="seperator" ></div>
         </div>
+        <div id="add-option" ></div>
         <div class="add-option pull-left"><h4><a ><img src="/img/add.png" alt="" style=""> ADD OPTION</a><h4>
         </div>
         <a  class="open-help-form pull-right" data-toggle="modal" href="#helpModal" target="_blank" class="social-list-facebook-edit open-help-form">
