9f65ff96e56a1cf6080fdc0ad68698506d559aa1
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-08 21:40:14 +0530

    videolink 1

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -30,6 +30,7 @@
           $venues = $campaign->venues;
           $pledges = $campaign->pledges;
           $campaign_desc = $campaign->campaign_desc;
+          $vlink = $campaign->videoLink;
           $fb = $campaign->fb;
           $twitter = $campaign->twitter;
           $scloud = $campaign->scloud;
@@ -42,7 +43,7 @@
       <div class="row d-tj-tour">
         <div class="col-sm-12 col-xs-12 col-md-7"> 
           <iframe title="YouTube video player" class="d-tj-video" width="535" height="300" 
-          src="http://www.youtube.com/embed/6X3zUh8RqbY?hd=1" frameborder="0" allowfullscreen></iframe>
+          src="<? print($vlink); ?>" frameborder="0" allowfullscreen></iframe>
         </div>  
         <div class="col-sm-12 col-md-5 d-tj-black-box-container" >
           <div class="d-tj-black-box d-tj-tour-right" > 
