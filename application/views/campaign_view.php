6bacdb64f9b8c3fcd8a2d11031de0a2d90884069
TommyJams <nikhilk218@gmail.com>
2013-10-14 19:57:20 +0530

    back image styling 18

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -47,7 +47,8 @@
     <div class="d-tj-box " >
       <div class="row d-tj-tour">
         <div class="col-sm-12 col-xs-12 col-md-7"> 
-          <iframe title="YouTube video player" class="d-tj-video" src="http://www.youtube.com/embed/<? print($vlink); ?>" frameborder="0" allowfullscreen></iframe>
+          <iframe title="YouTube video player" class="d-tj-video" style="min-height: 300px; width: 100%;" 
+          src="http://www.youtube.com/embed/<? print($vlink); ?>" frameborder="0" allowfullscreen></iframe>
         </div>  
         <div class="col-sm-12 col-md-5 d-tj-black-box-container" >
           <div class="d-tj-black-box d-tj-tour-right" > 
