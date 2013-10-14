db94f0086e0b64eb36ea0dbe586a1ca9a3c87206
TommyJams <nikhilk218@gmail.com>
2013-10-14 18:29:33 +0530

    removing error

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -253,14 +253,6 @@
 				 }
 );	
 
-$('#foo5').carouFredSel({
-	
-					width: '100%',
-					prev: '#prev5',
-					next: '#next5',
-					scroll: 1
-				});
-	});
 </script>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script> 
 <!--<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>-->
