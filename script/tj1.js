a5ee23468da9ba93edb87d2dd7710f01580b42c8
TommyJams <nikhilk218@gmail.com>
2013-10-14 20:45:34 +0530

    background image upload 2

--- script/tj1.js
+++ script/tj1.js
@@ -1,26 +1,5 @@
 // JavaScript Document
 
-/*Supersized*/	
-
-jQuery(function($){
-				
-				$.supersized({
-				
-					// Functionality
-					slide_interval          :   4000,		// Length between transitions
-					transition              :   1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-					transition_speed		:	500,		// Speed of transition
-															   
-					// Components							
-					slide_links				:	'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
-					slides 					:  	[			// Slideshow Images
-														{image : '/img/grey-background.png', title : 'Image Credit: Maria Kazvan', thumb : 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/kazvan-1.jpg', url : 'http://www.nonsensesociety.com/2011/04/maria-kazvan/'}
-			
-												]
-					
-				});
-		    });
-
 /*Text editor*/	
 	
 			  $(function(){
