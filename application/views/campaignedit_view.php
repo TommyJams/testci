39b2fa7d295212f55b320880d7ed83ba5ade3c93
TommyJams <nikhilk218@gmail.com>
2013-10-17 13:10:19 +0530

    upload video feedback 12

--- application/views/campaignedit_view.php
+++ application/views/campaignedit_view.php
@@ -134,7 +134,8 @@
         <div class="row d-tj-tour">
           <div class="col-sm-12 col-xs-12 col-md-7 d-tj-video-edit">
             <div class="d-tj-video-edit-bdr">
-              <div class="vd-link-selected"/>
+              <div class="vd-link-selected">
+              </div>
               <h2>
                 <a  data-toggle="modal" id="vd-link" href="#videoModal" title="VideoLink" alt="" target="_blank" 
                     class="social-list-facebook-edit open-video-link">UPLOAD<br>VIDEO LINK</a>
