46dbb75e07dd697169a1151dd9b62a3822c005dd
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-22 21:46:51 +0530

    css changes 3

--- application/views/campaignedit_view.php
+++ application/views/campaignedit_view.php
@@ -268,15 +268,9 @@
         <div class="pledge">
           <h4>
             <input  class="form-control input-lg pull-left" type="text" id="pledgeAmount1" name="pledgeAmount1" placeholder="PLEDGE AMT 1">
-          </h4>
-            <ul>
-                <li>
-                    <a class="open-help-form pull-right" data-toggle="modal" href="#helpModal" target="_blank" class="social-list-facebook-edit open-help-form"><img src="/img/help.png" alt="" style=""></a>
-                </li>
-                <li>
-                    <i class=" btn-delete-pledge pull-right"><a class="btn-delete-pledge-hover"></a></i>
-                </li>
-            </ul>  
+            <a class="open-help-form pull-middle" data-toggle="modal" href="#helpModal" target="_blank" class="social-list-facebook-edit open-help-form"><img src="/img/help.png" alt="" style=""></a>
+            <i class=" btn-delete-pledge pull-right"><a class="btn-delete-pledge-hover"></a></i>
+          </h4> 
           <div class="clearfix"></div>
           <h5>
             <textarea class="form-control" id="desc1" name="desc1" placeholder="Please write description" rows="4"></textarea>
