9e295c81f5747ede39c9f5a518c66c880b542d81
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-22 20:30:38 +0530

    Revert "fancybox 35"
    This reverts commit ab6c9433b429d9b4f47e7fbdf1159515ad50d56c.

--- application/views/campaignedit_view.php
+++ application/views/campaignedit_view.php
@@ -586,11 +586,11 @@ campaign_view -> hack
 
     });
 
-}   
+});    
 
   function usePledgeVal(type)
   {
-    //var maxIndex = 3;
+    var maxIndex = 3;
     if(type == "PledgeVal1")
     {
         maxIndex++;
@@ -753,8 +753,6 @@ campaign_view -> hack
       $('input[name=sociallink-3]').val(link);
     }
   }
-
-  ); 
 
 </script>
 </body>
