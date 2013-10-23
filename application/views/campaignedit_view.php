cda6c4d74932bfb9a8a7b4b497424f7a2a1c0969
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-23 16:17:40 +0530

    help box change 33

--- application/views/campaignedit_view.php
+++ application/views/campaignedit_view.php
@@ -267,9 +267,7 @@
         <div class="pledge">
           <h4>
             <input  class="form-control input-lg pull-left" type="text" id="pledgeAmount1" name="pledgeAmount1" placeholder="PLEDGE AMT 1">
-            <div>
-              <a class="open-help-form pull-right" data-toggle="modal" href="#helpModal" target="_blank" class="social-list-facebook-edit open-help-form"><img src="/img/help.png" alt="" style=""></a>
-            </div>
+            <div class="open-help-form pull-right"><a class="open-help-form-hover" data-toggle="modal" href="#helpModal" target="_blank" ></a></div>
             <div class=" btn-delete-pledge pull-right"><a class="btn-delete-pledge-hover"></a></div>
           </h4> 
           <div class="clearfix"></div>
