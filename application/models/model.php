349cdce7df7b729807fbab91068d91328e980b15
TommyJams <nikhilk218@gmail.com>
2013-10-23 23:08:11 +0530

    facebook event 68

--- application/models/model.php
+++ application/models/model.php
@@ -260,7 +260,7 @@ class Model extends CI_Model{
                                                                                    'scope' => 'rsvp_event',
                                                                                    'redirect_uri' => base_url().'campaign/'.$campaign_id
                                                                             ));
-                                   error_log('fbEventStatus:'.fbEventStatus);
+                                   error_log('Fb Status:'.$fbEventStatus);
                             }
 
 				$campaignDetails = array(
