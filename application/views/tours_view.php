6619beb7ae1559b5f929b97f08e1998fb78b1959
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-24 20:32:42 +0530

    tours page changes

--- application/views/tours_view.php
+++ application/views/tours_view.php
@@ -82,7 +82,7 @@
     <!--cirle connect-->
     <div class="d-tj-black-box d-tj-offset-top-40 d-tj-circle" >
       <h3>How it works?</h3>
-      <div class="visible-lg visible-md d-tj-bg-strip" ></div>
+      <div class="visible-lg visible-md d-tj-bg-strip d-tj-offset-top-40" ></div>
       <div class="row " >
         <div class="col-md-3 col-sm-6 text-center">
           <div class="d-tj-thumb img-circle" style="background-image:url(img/down/a.png)"></div>
