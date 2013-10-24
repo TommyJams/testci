a1ba6d2907978bef28ae95e63ecb633966b2bb82
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-24 23:30:06 +0530

    footer changes

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -231,9 +231,9 @@
       <div class="col-md-12 f-logo"> <a href="http://www.tommyjams.com/"><img src="/img/tj.jpg" height="50" alt=""> </a> </div>
       <div class="col-md-12 f-social">
         <ul class=" list-unstyled social-list clear-fix">
-          <li><a href="http://www.facebook.com/tommyjams.live" title="" alt="Facebook" target="_blank" class="social-list-facebook" data-original-title="Facebook"></a></li>
-          <li><a href="http://twitter.com/TommyJams" title="" alt="Twitter" target="_blank" class="social-list-twitter" data-original-title="Twitter"></a></li>
-          <li class="f-like">
+          <li><a style="margin-right:2px;margin-top:2px;" href="http://www.facebook.com/tommyjams.live" title="" alt="Facebook" target="_blank" class="social-list-facebook" data-original-title="Facebook"></a></li>
+          <li><a style="margin-right:2px;margin-top:2px;" href="http://twitter.com/TommyJams" title="" alt="Twitter" target="_blank" class="social-list-twitter" data-original-title="Twitter"></a></li>
+          <li style="margin-top:2px;" class="f-like">
             <iframe src="http://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Ftommyjams.live&amp;width=100&amp;height=21&amp;colorscheme=light&amp;layout=button_count&amp;action=like&amp;show_faces=true&amp;send=false&amp;appId=625320827487862" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe>
           </li>
         </ul>
