24fa928b2fb75d77d2de197fb1648a59b1eb21c9
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-25 00:25:23 +0530

    tweet text

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -149,7 +149,9 @@
           <div>
             <ul class=" list-unstyled social-list-share clear-fix">
               <li><a style="margin-right: 5px;" href="#" title="Share on Facebook" class='social-list-facebook-share' onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href), 'facebook-share-dialog', 'width=626,height=436'); return false;"></a></li>
-              <li><a href="#" title="Share on Twitter"  class='social-list-twitter-share'  onclick="window.open('https://twitter.com/share?u='+encodeURIComponent(location.href), 'width=626,height=436'); return false;" data-related="jasoncosta" data-lang="en" data-text="<?print($artist_name);?> is touring with TommyJams and coming to a venue near you. Pre-book your tickets now! \nTarget Sales: <? print($target); ?>" data-size="large" data-count="none"></a></li>
+              <li><a title="Share on Twitter"  class='social-list-twitter-share' href="https://twitter.com/share" data-url="https://dev.twitter.com/pages/tweet_button" data-via="your_screen_name" data-lang="en" data-text="<?print($artist_name);?> is touring with TommyJams and coming to a venue near you. Pre-book your tickets now! \nTarget Sales: <? print($target); ?>" data-related="anywhereTheJavascriptAPI" data-count="vertical"></a>
+
+              <!--  <a href="#" title="Share on Twitter"  class='social-list-twitter-share'  onclick="window.open('https://twitter.com/share?u='+encodeURIComponent(location.href), 'width=626,height=436'); return false;" data-related="jasoncosta" data-lang="en" data-text="<?print($artist_name);?> is touring with TommyJams and coming to a venue near you. Pre-book your tickets now! \nTarget Sales: <? print($target); ?>" data-size="large" data-count="none"></a></li>-->
             </ul>
             <div class="clearfix"></div>
             <?
