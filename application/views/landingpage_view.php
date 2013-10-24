5c77190622bc68823a77489243e3ba0ce5d98103
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-24 15:30:11 +0530

    Revert "twitter feed change 3"
    This reverts commit 3a6f453485af44051b9cf6dd8c59d36c964bd605.

--- application/views/landingpage_view.php
+++ application/views/landingpage_view.php
@@ -360,15 +360,7 @@
 
       $('#latest-tweets').append(list);
 
-      $("#latest-tweets ul").jCarouselLite({
-        vertical: true,
-        hoverPause:true,
-        visible: 1,
-        auto:2000,
-        speed:1000
-  });
-
-   /*   $('#latest-tweets ul').carouFredSel(
+      $('#latest-tweets ul').carouFredSel(
       {
         circular    : true,
         direction   : 'up',
@@ -383,7 +375,7 @@
           duration  : 750,
           fx      : 'cover'
         }
-      }); */
+      }); 
       
       setTwitterDimension();
     }
