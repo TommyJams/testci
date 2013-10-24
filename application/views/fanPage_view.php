fb1379c13dc7fb5f2e7fd8a332518d6feb0a09e7
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-24 19:26:54 +0530

    fans page text change 2

--- application/views/fanPage_view.php
+++ application/views/fanPage_view.php
@@ -26,22 +26,22 @@
       <div class="row d-tj-offset-top-40">
         <div class="col-md-3 col-sm-6 text-center">
           <div class="d-tj-thumb img-circle" style="background-image:url(img/up/1.png)"></div>
-          <h5 ><b>Decrease Costs<b><br>
+          <h5 ><b>Decrease Costs</b><br>
             No more paying for entry tickets, get entry into all the shows once you pledge your support.</h5>
         </div>
         <div class="col-md-3 col-sm-6 text-center">
           <div class="d-tj-thumb img-circle" style="background-image:url(img/up/2.png)"></div>
-          <h5 ><b>Freebies<b><br>
+          <h5 ><b>Freebies</b><br>
             Get freebies every time you pledge. It’s your chance to directly talk to that favorite guitarist of yours.</h5>
         </div>
         <div class="col-md-3 col-sm-6 text-center">
           <div class="d-tj-thumb img-circle" style="background-image:url(img/up/3.png)"></div>
-          <h5><b>Pay for the Music<b><br>
+          <h5><b>Pay for the Music</b><br>
             Pay for what you came for and support your favorite artists directly.</h5>
         </div>
         <div class="col-md-3 col-sm-6 text-center">
           <div class="d-tj-thumb img-circle" style="background-image:url(img/up/4.png)"></div>
-          <h5 ><b>Revolution<b><br>
+          <h5 ><b>Revolution</b><br>
             Be part of a new age in music, where the fans connect directly with the artists.</h5>
         </div>
       </div>
@@ -104,7 +104,7 @@
       <div class="who-campaigns">
         <h3>WHO CAMPAIGNS ?</h3> 
         <h5>All the campaigns are initiated by the artists directly for the tours that they want to venture on. Every time you pledge your support, an artist gets one step closer to his dream tour across various cities in the country. Not only that, you get exclusive access to all the shows which are part of that tour, and even get extra freebies from the band on-the-day to make the event really special for you.
-            <b>Start pledging now!<b><br>
+            <b>Start pledging now!</b><br><br>
             In case you are an artist and want to start a campaign of your own, learn how to do it by simply visiting our available tours page.</h5>
         <div class="text-center ">
         <input class="apply-btn" style="" onClick="window.location.href='/tours'" type="button" value="LEARN MORE">
