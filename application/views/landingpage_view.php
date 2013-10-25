3de82d85e3187b7a355c818a81d5e751c48f472f
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-25 23:02:04 +0530

    landing page change

--- application/views/landingpage_view.php
+++ application/views/landingpage_view.php
@@ -82,9 +82,9 @@
         <div class="col-md-6 d-tj-book-events" >
           <div class="col-md-12 d-tj-book-events-bg" >
             <div class="d-tj-book-events-top"  > 
-              <h4 class="text-center">
+              <h2 class="text-center">
                 Looking to book events immediately ?
-              </h4>
+              </h2>
             </div>
             <div class="text-center d-tj-offset-top-20">
               <input onClick="window.location.href='/index'" type="button" value="BOOK EVENTS">
@@ -96,6 +96,9 @@
             <div class="d-tj-c-tour-top"  > 
               <h4 class="text-center">
                 Looking to campaign for cross-city tours ?
+              </h4>
+              <h4>
+                <img src="/img/test_icon.jpg" alt="" >
               </h4>
             </div>
             <div class="text-center d-tj-offset-top-20">
