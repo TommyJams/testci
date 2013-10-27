6deb593678e34f5f486cacf32de51dd5327a661b
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-27 17:05:15 +0530

    tabs 1

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -275,11 +275,13 @@
                   <? 
                     } 
                   ?>
-                  <iframe style="display:inline-block" frameborder="0" src="http://em.explara.com/widget/test-ci-event" width="315" ></iframe>
+                  <iframe style="display:inline-block" frameborder="0" src="http://em.explara.com/widget/test-ci-event" height="700" width="315" ></iframe>
                 </div>
               </div>
               <div class="tab-pane fade" id="profile">
-                <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</p>
+                <div class="d-tj-pledge">
+                  <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</p>
+                </div>
               </div>
             </div>
             <!--/Tabs -->
