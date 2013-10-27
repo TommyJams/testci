197856e030f61309dae83b98847d10ce648b118d
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-27 14:47:49 +0530

    explara event

--- application/views/campaign_view.php
+++ application/views/campaign_view.php
@@ -268,7 +268,9 @@
                 } 
               ?>
             </div>
+            <div class=" d-tj-offset-top-30 pledge-btn" >
             <iframe frameborder="0" src="http://em.explara.com/widget/test-ci-event" width="300" height="110"></iframe>
+            </div>
             <div class=" d-tj-offset-top-30 pledge-btn" >
               <input type="button" value="PLEDGE NOW" style="">
             </div>
