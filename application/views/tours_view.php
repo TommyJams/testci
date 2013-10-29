4042718f1cb7c03c56b665b516347f2a156f9cb5
TommyJams <tommyjams.bizspark@outlook.com>
2013-10-29 17:39:56 +0530

    footer change

--- application/views/tours_view.php
+++ application/views/tours_view.php
@@ -264,65 +264,13 @@
     </div>
   </div>
     
-    <footer class="d-tj-footer">
-    <div class="col-md-12 col-sm-12 col-xs-12 footer-main">
-      <div class="col-md-3 col-sm-12 col-xs-12 footer-top">
-        <div class="col-md-12 f-logo"> <a href="http://www.tommyjams.com/"><img src="img/tj.jpg" height="50" alt=""> </a> </div>
-        <div class="col-md-12 f-social">
-          <ul class=" list-unstyled social-list clear-fix">
-            <li><a style="margin-right:2px;margin-top:2px;" href="http://www.facebook.com/tommyjams.live" title="" alt="Facebook" target="_blank" class="social-list-facebook" data-original-title="Facebook"></a></li>
-            <li><a style="margin-right:2px;margin-top:2px;" href="http://twitter.com/TommyJams" title="" alt="Twitter" target="_blank" class="social-list-twitter" data-original-title="Twitter"></a></li>
-            <li style="margin-top:2px;" class="f-like">
-              <iframe src="http://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Ftommyjams.live&amp;width=100&amp;height=21&amp;colorscheme=light&amp;layout=button_count&amp;action=like&amp;show_faces=true&amp;send=false&amp;appId=625320827487862" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="col-md-6 col-sm-12 col-xs-12 f-links ">
-        <div class="col-md-3 col-sm-3 col-xs-3 f-artist text-center">
-          <ul class="list-unstyled">
-            <li>
-              <h4>ARTIST</h4>
-            </li>
-            <li> <a href="/tours">GRAB TOURS</a> </li>
-          <li> <a href="/index">BOOK EVENTS</a> </li>
-        </ul>
-      </div>
-      <div class="col-md-3 col-sm-3 col-xs-3 f-venues text-center">
-        <ul class="list-unstyled">
-          <li>
-            <h4 >VENUES</h4>
-          </li>
-          <li> <a href="/index">BOOK ARTIST</a> </li>
-        </ul>
-      </div>
-      <div class="col-md-3 col-sm-3 col-xs-3 f-fans text-center">
-        <ul class="list-unstyled">
-          <li>
-            <h4 >FANS</h4>
-          </li>
-          <li> <a href="/fans">CAMPAIGN</a> </li>
-          <li> <a href="/radioone">RADIO ONE</a> </li>
-        </ul>
-      </div>
-      <div class="col-md-3 col-sm-3 col-xs-3 f-help text-center">
-        <ul class="list-unstyled">
-          <li>
-            <h4 >HELP</h4>
-          </li>
-          <li> <a href="/help">CONTACT US</a> </li>
-          <li> <a href="/aboutus">ABOUT US</a> </li>
-          <li> <a href="/press">PRESS</a> </li>
-          <li> <a href="/advertise">ADVERTISE</a> </li>
-          </ul>
-        </div>
-      </div>
-      <div class="col-md-3 col-sm-12 col-xs-12 f-ms"> <a><img src="img/m-s-ventures.png" style=""/></a> </div>
+    <!-- Footer  -->      
     </div>
-    <div class="col-md-12 col-sm-12 col-xs-12 f-copy">
-      <p>COPYRIGHT 2013 - ALL RIGHTS RESERVED</p>
-    </div>
-  </footer>
+    <?
+      include("include/footer.php");
+    ?>
+    <!-- /Footer  -->
+    
 </div>
 <script src="/script/jquery.js"></script> 
 <script src="/script/bootstrap.min.js"></script> 
